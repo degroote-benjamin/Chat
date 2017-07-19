9a93b3aed2041027b5f59a3ffcabefee6207d37c
benjamin <degroote.benjamin@gmail.com>
2017-07-19 15:52:38 +0200

    add function for add on the div reponse the answer user

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 var robot = ["bonjour","++","bonjour","ca va?","il fait chaud"]
+var compt = 0
+
+
+$(document).ready(function(){
+
+    $('.submit').on('click',function(){
+       $('.reponse').append('<p class="text-right">dadza</p>')
+    });
+
+});
