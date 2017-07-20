fe771431bbd6d7df74084dc9220f2d0c37fd5df0
benjamin <degroote.benjamin@gmail.com>
2017-07-20 14:34:40 +0200

    clean when message in send and enter keypress is ok

--- js/main.js
+++ js/main.js
@@ -3,18 +3,30 @@ var compt = 0
 
 
 $(document).ready(function() {
-
     $('.submit').on('click', function() {
-        if (compt === robot.length - 1) {
-            compt = 0
-        }
-        $('.reponse').append('<p class="text-right">' + $(".pseudo").val() + ': ' + $(".texte").val() + '</p>')
-        $(".reponse").scrollTop($(".reponse").height() + 1000000)
-        setTimeout(function() {
-            $('.reponse').append('<p class="text-left">' + "robot : " + robot[compt] + '</p>')
-            $(".reponse").scrollTop($(".reponse").height() + 1000000)
-        }, 1000);
-        compt++
+chat()
     });
 
 });
+
+
+$(".texte").keypress(function(e) {
+    if(e.which == 13) {
+chat()
+}
+  });
+
+
+function chat(){
+    if (compt === robot.length - 1) {
+        compt = 0
+    }
+    $('.reponse').append('<p class="text-right">' + $(".pseudo").val() + ': ' + $(".texte").val() + '</p>')
+    $(".reponse").scrollTop($(".reponse").height() + 1000000)
+    setTimeout(function() {
+        $('.reponse').append('<p class="text-left">' + "robot : " + robot[compt] + '</p>')
+        $(".reponse").scrollTop($(".reponse").height() + 1000000)
+    }, 1000);
+    $(".texte").val("")
+    compt++
+}
