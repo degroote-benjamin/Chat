23a6442c51eda1a00020064050e06b2803ba96d2
benjamin <degroote.benjamin@gmail.com>
2017-07-20 09:40:58 +0200

    autoscroll bot

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
-var robot = ["bonjour","++","bonjour","ca va?","il fait chaud"]
+var robot = ["bonjour", "++", "bonjour", "ca va?", "il fait chaud"]
 var compt = 0
 
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $('.submit').on('click',function(){
-      if(compt === robot.length-1){
-        compt=0
-      }
-      $('.reponse').scrollTop = $('.reponse').scrollHeight;
-       $('.reponse').append('<p class="text-right">'+$(".pseudo").val() +': '+$(".texte").val() +'</p>')
-       setTimeout(function () {
-       $('.reponse').append('<p class="text-left">'+"robot : " + robot[compt] +'</p>')
-  }, 1000);
-       compt++
+    $('.submit').on('click', function() {
+        if (compt === robot.length - 1) {
+            compt = 0
+        }
+        $('.reponse').append('<p class="text-right">' + $(".pseudo").val() + ': ' + $(".texte").val() + '</p>')
+        $(".reponse").scrollTop($(".reponse").height() + 1000000)
+        setTimeout(function() {
+            $('.reponse').append('<p class="text-left">' + "robot : " + robot[compt] + '</p>')
+            $(".reponse").scrollTop($(".reponse").height() + 1000000)
+        }, 1000);
+        compt++
     });
 
 });
