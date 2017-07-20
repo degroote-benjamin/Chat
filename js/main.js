4ce10e622ce6d13cbf74338ae9148649057af793
benjamin <degroote.benjamin@gmail.com>
2017-07-20 09:02:23 +0200

    add color for each player

--- js/main.js
+++ js/main.js
@@ -5,7 +5,15 @@ var compt = 0
 $(document).ready(function(){
 
     $('.submit').on('click',function(){
-       $('.reponse').append('<p class="text-right">dadza</p>')
+      if(compt === robot.length-1){
+        compt=0
+      }
+      $('.reponse').scrollTop = $('.reponse').scrollHeight;
+       $('.reponse').append('<p class="text-right">'+$(".pseudo").val() +': '+$(".texte").val() +'</p>')
+       setTimeout(function () {
+       $('.reponse').append('<p class="text-left">'+"robot : " + robot[compt] +'</p>')
+  }, 1000);
+       compt++
     });
 
 });
